43a7380cc857ed2ad2665fbc75a8888951e08b82
elahe sabetinia <saikikusou35@gmail.com>
2024-10-31 20:52:22 +0330

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("enter only number(for exist press 'Q' key) ");
+
+        while (true)
+        {
+            // خواندن کلید از کاربر
+            ConsoleKeyInfo keyInfo = Console.ReadKey(intercept: true); // intercept: true از نمایش خودکار جلوگیری می‌کند
+
+            // بررسی کلید وارد شده برای خروج
+            if (keyInfo.Key == ConsoleKey.Q)
+            {
+                Console.WriteLine("\nexist from code6.");
+                break;
+            }
+
+            // بررسی اینکه آیا کلید وارد شده یک عدد است
+            if (char.IsDigit(keyInfo.KeyChar))
+            {
+                // نمایش عدد در خروجی
+                Console.Write(keyInfo.KeyChar);
+            }
+        }
+    }
+}
